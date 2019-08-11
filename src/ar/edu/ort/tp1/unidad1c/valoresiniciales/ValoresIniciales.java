1da867fb3b2cfb09e6e17e176654d89fe61725fc
Marco Cupo <cuttlas88@yahoo.com.ar>
2019-08-11 19:43:45 -0300

    Ejemplo de array nativo de chars

--- src/ar/edu/ort/tp1/unidad1c/valoresiniciales/ValoresIniciales.java
+++ src/ar/edu/ort/tp1/unidad1c/valoresiniciales/ValoresIniciales.java
@@ -7,6 +7,7 @@ public class ValoresIniciales {
 	private static final int CANT_ALUMNOS	= 6;
 	
 	private static int 	   [] decena 		= new int[DECENA];
+	private static char	   [] abecedario	= new char[26];
 	private static boolean [] aprobados 	= new boolean[CANT_ALUMNOS];
 	private static Integer [] mediaDecena 	= new Integer[MEDIA_DECENA];
 	
@@ -16,6 +17,13 @@ public class ValoresIniciales {
 		System.out.println("Valores para el array decena");
 		for(int i = 0; i < decena.length; i++){
 			System.out.println("Valor en posicion " + i + ": " + decena[i]);
+		}
+		
+		//JAVA inicializa en el caracter null (\u0000) a los arrays de tipo char
+		//Por consola veremos un cuadrado, esto es para indicar que no es un caracter que pueda ser mostrado
+		System.out.println("Valores para el array abecedario");
+		for(int i = 0; i < abecedario.length; i++){
+			System.out.println("Valor en posicion " + i + ": " + abecedario[i]);
 		}
 		
 		//JAVA inicializa en false a los arrays de tipo boolean
